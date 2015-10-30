1f9b27cc483b00c74d6f9d21968ae3e4dfd7c55a
Nick Sieger <nick@nicksieger.com>
2015-10-30 12:29:27 -0500

    swagger_yard_spec.rb: expand checks instead of comparing to big json blob

--- spec/integration/swagger_yard_spec.rb
+++ spec/integration/swagger_yard_spec.rb
@@ -1,43 +1,66 @@
 require 'spec_helper'
 
 describe SwaggerYard, '.generate' do
-  let(:app_path) {'../../fixtures/dummy/app/'}
+  let(:app_path)        {'../../fixtures/dummy/app/'}
+  let(:model_path)      {File.expand_path(app_path+'models/**/*.rb', __FILE__)}
+  let(:controller_path) {File.expand_path(app_path+'controllers/**/*.rb', __FILE__)}
 
-  context "for a valid controller" do
-    let(:model_path) {File.expand_path(app_path+'models/*.rb', __FILE__)}
-    let(:controller_path) {File.expand_path(app_path+'controllers/*.rb', __FILE__)}
+  subject(:resource_listing) { SwaggerYard::ResourceListing.new(controller_path, model_path) }
 
-    let(:resource_listing) {SwaggerYard::ResourceListing.new(controller_path, model_path)}
+  context "json" do
+    subject { resource_listing.to_h }
 
-    let(:api_json) {File.read(File.expand_path('../../fixtures/api.json', __FILE__))}
+    it { is_expected.to include("paths", "definitions", "tags", "securityDefinitions") }
+  end
 
-    it 'generates swagger api json for the given controllers and models' do
-      expect(resource_listing.to_h).to eq(JSON.parse(api_json))
+  context "paths" do
+    subject { resource_listing.to_h["paths"] }
+
+    its(:size) { is_expected.to eq(4) }
+
+  end
+
+  context "tags" do
+    subject(:tags) { resource_listing.to_h["tags"] }
+
+    specify "have all resource tags" do
+      expect(tags.map{|h| h["name"]}).to eq(["Pet", "Transport"])
     end
+  end
 
-    # let(:pets_json) {File.read(File.expand_path('../../fixtures/pets.json', __FILE__))}
+  context "definitions" do
+    subject(:definitions) { resource_listing.to_h["definitions"] }
 
-    # it 'generates swagger json for an individual controller' do
-    #   expect(resource_listing.declaration_for('/pets').to_h).to eq(JSON.parse(pets_json))
-    # end
+    its(:keys) { are_expected.to eq(["AnimalThing", "Pet", "Possession", "Transport"]) }
 
-    # let(:transports_json) {File.read(File.expand_path('../../fixtures/transports.json', __FILE__))}
+    its(["AnimalThing", "properties"]) { are_expected.to include("id", "type", "possessions") }
 
-    # it 'generates swagger json for a controller with a response_type model' do
-    #   expect(resource_listing.declaration_for('/transports').to_h).to eq(JSON.parse(transports_json))
-    # end
+    its(["Pet", "properties"]) { are_expected.to include("id", "names", "age", "relatives") }
+
+    its(["Possession", "properties"]) { are_expected.to include("name", "value") }
+
+    its(["Transport", "properties"]) { are_expected.to include("id", "wheels") }
+  end
+
+  context "securityDefinitions" do
+    subject(:security) { resource_listing.to_h["securityDefinitions"] }
+
+    its(:keys) { are_expected.to eq(["header_x_application_api_key"]) }
+
+    its(["header_x_application_api_key"]) {
+      is_expected.to eq({ "type" => "apiKey",
+                          "name" => "X-APPLICATION-API-KEY",
+                          "in"   => "header" })
+    }
   end
 
   context "for non-controllers (modules) in the path" do
-    let(:controllers_path) {File.expand_path("#{app_path}/controllers/**/*.rb", __FILE__)}
-    let(:resources) { SwaggerYard::ResourceListing.new(controllers_path, nil) }
-
     it 'does not error' do
-      expect { resources.to_h }.to_not raise_error
+      expect { resource_listing.to_h }.to_not raise_error
     end
 
     it 'does not create resources that are not tagged as @resource' do
-      expect(resources.controllers.detect {|c|
+      expect(resource_listing.controllers.detect {|c|
                c.description =~ /NotAResourceController/ }).to be_nil
     end
   end
