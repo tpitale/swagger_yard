121a6b16c3506900b56a479b43390e687403a8df
Nick Sieger <nick@nicksieger.com>
2016-10-07 15:05:58 -0500

    Make Type use new parser, and handle keyword types case-insensitively

--- lib/swagger_yard/type.rb
+++ lib/swagger_yard/type.rb
@@ -1,107 +1,34 @@
 module SwaggerYard
   class Type
     def self.from_type_list(types)
-      parts = types.first.split(/[<>]/)
-      name = parts.last
-      options = {}
-      if parts.size > 1
-        case parts.first
-        when /^array$/i
-          options[:array] = parts[1..-1]
-        when /^enum$/i
-          name = nil
-          options[:enum] = parts.last.split(/[,|]/)
-        when /^regexp?$/i
-          name = 'string'
-          options[:pattern] = parts.last
-        when /^object$/i
-          options[:object] = parts[1..-1]
-        else
-          name = parts.first
-          options[:format] = parts.last
-        end
-      end
-      new(name, options)
+      new(types.first)
     end
 
-    attr_reader :name, :array, :enum, :object
+    attr_reader :name, :source, :schema
 
-    def initialize(name, options = {})
-      @name    = Model.mangle(name) if name
-      @array   = options[:array]
-      @enum    = options[:enum]
-      @format  = options[:format]
-      @pattern = options[:pattern]
-      @object  = options[:object]
+    def initialize(string)
+      @source  = string
+      @schema  = TypeParser.new.json_schema(string)
+      @name    = name_for(@schema)
+      @name    = name_for(@schema['items']) if @name == 'array'
     end
 
     # TODO: have this look at resource listing?
     def ref?
-      /[[:upper:]]/.match(name)
+      schema["$ref"]
     end
 
     def model_name
       ref? ? name : nil
     end
 
-    alias :array? :array
-    alias :enum? :enum
-    alias :object? :object
-
-    def json_type
-      type, format = name, @format
-      case name
-      when "float", "double"
-        type = "number"
-        format = name
-      when "date-time", "date", "time", "uuid"
-        type = "string"
-        format = name
-      end
-
-      hsh = { "type" => type }
-      hsh["format"] = format if format
-      hsh["pattern"] = @pattern if @pattern
-      hsh
-    end
-
     def to_h
-      type = if ref?
-        { "$ref" => "#/definitions/#{name}"}
-      elsif enum?
-        { "type" => "string", "enum" => @enum }
-      else
-        json_type
-      end
-
-      if array?
-        { "type" => "array", "items" => Type.from_type_list([array.join("<")]).to_h }
-      elsif object?
-        parse_object
-      else
-        type
-      end
+      schema
     end
 
-    def parse_object
-      properties = {}
-      additional = nil
-      type_hash  = { "type" => "object" }
-
-      object.join("<").split(/,\s?/).each do |item|
-        key, *rest = item.split(": ")
-
-        # TODO: Should we raise an error if more than one additional type?
-        if rest.empty?
-          additional = Type.from_type_list([key]).to_h
-        else
-          properties[key] = Type.from_type_list([rest.join(": ")]).to_h
-        end
-      end
-
-      type_hash["properties"] = properties unless properties.empty?
-      type_hash["additionalProperties"] = additional unless additional.nil?
-      type_hash
+    private
+    def name_for(schema)
+      schema["type"] || schema["$ref"][%r'#/definitions/(.*)', 1]
     end
   end
 end
