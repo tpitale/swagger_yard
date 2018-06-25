1a89fd9cb4209a848b494d9384714b67fa9c8c58
Nick Sieger <nick@nicksieger.com>
2018-06-25 13:56:09 -0500

    These are not used

--- lib/swagger_yard/configuration.rb
+++ lib/swagger_yard/configuration.rb
@@ -3,7 +3,6 @@ module SwaggerYard
     attr_accessor :api_version, :api_base_path
     attr_accessor :swagger_version
     attr_accessor :title, :description
-    attr_accessor :enable, :reload
     attr_accessor :controller_path, :model_path
     attr_accessor :path_discovery_function
     attr_accessor :security_definitions
@@ -11,8 +10,6 @@ module SwaggerYard
     def initialize
       @swagger_version = "2.0"
       @api_version = "0.1"
-      @enable = false
-      @reload = true
       @title = "Configure title with SwaggerYard.config.title"
       @description = "Configure description with SwaggerYard.config.description"
       @security_definitions = {}
