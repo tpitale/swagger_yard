e0cd147ecab6d30dfa217984db7020b860a5a3d7
Nick Sieger <nick@nicksieger.com>
2018-06-27 11:04:16 -0500

    Alias security_definitions to security_schemes

--- lib/swagger_yard/configuration.rb
+++ lib/swagger_yard/configuration.rb
@@ -7,6 +7,9 @@ module SwaggerYard
     attr_accessor :path_discovery_function
     attr_accessor :security_definitions
 
+    alias security_schemes security_definitions
+    alias security_schemes= security_definitions=
+
     def initialize
       @swagger_version = "2.0"
       @api_version = "0.1"
