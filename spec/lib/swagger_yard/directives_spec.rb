9f8e2fd6d14ac015d0d26fc92056c9c2bbf0d455
Quentin Wentzler <quentin.wentzler@klaxit.com>
2020-04-21 18:25:06 +0200

    Add @!model directive to declare model without an associated class
    This directive generate a @model tag with a dummy class taking the name
    given to the directive. All tags given to the directive are passed to the
    generated @model.

---
+++ spec/lib/swagger_yard/directives_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe SwaggerYard::Directives do
+  describe SwaggerYard::Directives::ParamClassDirective do
+    describe "#call" do
+      after { YARD::Registry.clear }
+
+      it "create ClassObject with @model tag" do
+        YARD.parse_string <<-EOF
+          # @!model Foo
+        EOF
+        yard_object = YARD::Registry.at("Foo")
+
+        expect(yard_object).to be_a(YARD::CodeObjects::ClassObject)
+        expect(yard_object.tags.first.tag_name).to eq("model")
+      end
+
+      it "create ClassObject with @model and @property" do
+        YARD.parse_string <<-EOF
+          # @!model Foo
+          # @property id
+          # @property name
+        EOF
+        yard_object = YARD::Registry.at("Foo")
+        
+        %w(id name).each do |property|
+          expect(yard_object.tags.find do |tag|
+            tag.tag_name == "property" && tag.name == property
+          end).to_not be_nil
+        end
+      end
+
+      it "create multiple ClassObject" do
+        YARD.parse_string <<-EOF
+          # @!model Foo
+          # @!model Boo
+        EOF
+
+        %w(Foo Boo).each do |model|
+          yard_object = YARD::Registry.at(model)
+          expect(yard_object).to be_a(YARD::CodeObjects::ClassObject)
+          expect(yard_object.tags.first.tag_name).to eq("model")
+        end
+      end
+    end
+  end
+end
